203a0ba872490ccc6d03c470406a301a225d2116
boedy <boudewijng@gmail.com>
2022-12-21 01:16:14 +0100

    copy namespace from kustomization.yaml and apply to helm template call

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,9 +2,14 @@
 HOME=/tmp
 
 if grep -q "\- \"Chart.yaml\"" ../../base/kustomization.yaml; then
+
+  BASE_NAMESPACE=$(cat ../../base/kustomization.yaml | yq e '.namespace // "default"' -)
+  NAMESPACE=$(cat kustomization.yaml | yq e ".namespace // \"$BASE_NAMESPACE\"" -)
+
   helm template \
     --release-name ${ARGOCD_APP_NAME} \
     --include-crds \
+    --namespace $NAMESPACE \
     ../../base \
     -f ../../base/values.yaml \
     -f values.yaml > ../../base/compiled-chart.yaml
